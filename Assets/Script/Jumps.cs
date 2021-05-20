660dac0ce4026ac1c82fc1c817dcb2ac4ae6c45c
ismaelkamahate <39011511+ismaelkamahate@users.noreply.github.com>
2021-05-20 17:41:56 -0400

    final Project

---
+++ Assets/Script/Jumps.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumps : MonoBehaviour
+{
+    public float jumph;
+    public float jumpforce;
+    private Vector3 jump;
+    private Rigidbody rigg;
+
+    private bool isgrounded; //new
+
+    void Start()
+    {
+        jump = new Vector3(0f, jumph, 0f);
+        rigg = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Space) && isgrounded)
+        {
+            rigg.AddForce(jump * jumpforce, ForceMode.Impulse);
+            isgrounded = false;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "floor")
+            isgrounded = true;
+    }
+}
---
+++ Assets/Script/MOVENMENT.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MOVENMENT : MonoBehaviour
+{
+    Animator SIR;
+
+    private Rigidbody rb;
+    public Vector3 fwd, bwd, lft, rgt;
+    private int count;
+    public TextMeshProUGUI countText;
+
+    // Start is called before the first frame update
+    void Start() //like setup
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+
+        SIR = GetComponent<Animator>();
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SIR.SetInteger("D", 0);
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += lft;
+            SIR.SetInteger("D", 2);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += rgt;
+            SIR.SetInteger("D", 1);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+}
